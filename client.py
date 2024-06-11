82913662966732076d91614740fb212fc7349bdc
root <root@ubuntu-20-isp.ruvds.com>
2024-06-11 23:38:49 +0300

    Add  text_to speech

--- client.py
+++ client.py
@@ -1,5 +1,9 @@
 import requests
 import speech_recognition as sr
+from gtts import gTTS
+import os
+import time
+
 
 # Function to record audio
 def record_audio(recognizer, microphone, timeout_duration):
@@ -25,16 +29,29 @@ def recognize_speech(recognizer, audio):
         print("Не получилось запросить результаты")
         return None
 
+def text_to_speech(text, lang='ru'):
+
+    tts = gTTS(text, lang=lang)
+
+    tts.save("output.mp3")
+
+    os.system("mpg321 output.mp3") # Use 'mpg321' to play the audio
+
+
 # Main function to send request
 def send_request(user_content):
     url = "http://194.59.40.99:7990/process_text/"
-    system_content = "Вы психолог, специализирующийся на детях"
+    system_content = "Вы умная колонка, созданная Шамилем. Отвечай на русском языке."
 
     payload = {"system_content": system_content, "user_content": user_content}
     headers = {"Content-Type": "application/json"}
 
     response = requests.post(url, json=payload, headers=headers)
-    print(response.json()["response"])
+    message_txt = response.json()["response"]
+    print(message_txt)
+    time.sleep(1)
+
+    text_to_speech(message_txt)
 
 # Main code
 if __name__ == "__main__":
